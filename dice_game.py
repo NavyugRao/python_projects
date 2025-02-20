dd7e0a693d07f32aaeed6631d3f616bb4c8b0860
NavyugRao <Navyugrajofficial@gmail.com>
2025-02-21 04:02:56 +0530

    Create dice_game.py
    a dice game which shows a random number by showing corresponding dice. made of _|*

---
+++ dice_game.py
@@ -0,0 +1,32 @@
+import random
+
+def print_dice(number):
+    print(" _______ ")
+    
+    if number == 1:
+        print("|       |")
+        print("|   *   |")
+        print("|_______|")
+    elif number == 2:
+        print("| *     |")
+        print("|       |")
+        print("|_____ *|")
+    elif number == 3:
+        print("| *     |")
+        print("|   *   |")
+        print("|_____ *|")
+    elif number == 4:
+        print("| *   * |")
+        print("|       |")
+        print("|_*___*_|")
+    elif number == 5:
+        print("| *   * |")
+        print("|   *   |")
+        print("|_*___*_|")
+    elif number == 6:
+        print("| *   * |")
+        print("| *   * |")
+        print("|_*___*_|")
+
+dice_value = random.randint(1, 6)
+print_dice(dice_value)
